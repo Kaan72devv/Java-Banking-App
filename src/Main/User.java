a2d98e87cc0cfa51f8cea2da52833fae77b4e71f
Kaan Canlıoğlu <kaancanlioglu71@gmail.com>
2026-05-25 15:10:31 +0300

    Create User.java

---
+++ src/Main/User.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class User {
+
+    public String username;
+    public String password;
+    public double balance;
+
+    public User(String username, String password, double balance) {
+
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+}
